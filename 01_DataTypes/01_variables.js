fe845cbabb9c657c2125d0174afd61dcd8c96ef7
Sidhanta Dash <65765885+SiddhantDash6@users.noreply.github.com>
2023-09-16 06:34:27 +0000

    Conversion Of DataTypes

--- 01_DataTypes/01_variables.js
+++ 01_DataTypes/01_variables.js
@@ -1,2 +1,24 @@
-let x = 5;
-console.log(x);
+let score = "33";
+let score2 = "33abc";   // if also "undefined", Number conversion will give "NaN"
+let score3 = null;
+
+let valueInInt = Number(score);
+let valueInInt2 = Number(score2);
+let valueInInt3 = Number(score3);
+
+// console.log(typeof valueInInt);
+// console.log(valueInInt);
+
+// console.log(typeof valueInInt2);
+// console.log(valueInInt2);
+
+// console.log(typeof valueInInt3);
+// console.log(valueInInt3);
+
+let isLoggedIn = 1
+let isBooleanLoggedIn = Boolean(isLoggedIn)
+console.log(isBooleanLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "sidhanta" => true
